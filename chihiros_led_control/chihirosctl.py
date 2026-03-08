b5101188d7fa961d7544c5e9fd95b9879f4d1c77
Rob Campbell <mantid@gmail.com>
2026-03-08 14:05:47 -0700

    Fix model_code attribute error in list-devices

--- chihiros_led_control/chihirosctl.py
+++ chihiros_led_control/chihirosctl.py
@@ -46,8 +46,8 @@ def list_devices(timeout: Annotated[int, typer.Option()] = 5) -> None:
         model_name = "???"
         if device.name is not None:
             model_class = get_model_class_from_name(device.name)
-            if model_class.model_code:  # type: ignore
-                model_name = model_class.model_name  # type: ignore
+            if model_class._model_codes:
+                model_name = model_class._model_name or "???"
         table.add_row(device.name, device.address, model_name)
     print("Discovered the following devices:")
     print(table)
